a8e98ccdded77dd2d63ce81eb3440e8b7e51411e
muthukrishnnan2812 <muthumk2812@gmail.com>
2026-06-03 21:31:55 +0530

    table name change

--- src/main/java/com/example/Banking_Application/Entity/User.java
+++ src/main/java/com/example/Banking_Application/Entity/User.java
@@ -12,10 +12,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
+@Table(name = "users")
 public class User {
 	
 	@Id
